84018f02ed2eb3fcbb7107c55a3c22e0eaa264d8
Emmanuel <egoossaert+github+commit@gmail.com>
2022-06-19 20:50:55 +0200

    Cleanup.

--- backend/middleware/auth.middleware.ts
+++ backend/middleware/auth.middleware.ts
@@ -12,14 +12,6 @@ export const AuthHandler = (
     const token = request.headers.authorization.split(" ")[1];
     const decodedData: any = jwt.verify(token, JWT_SECRET_SALT);
 
-    if (!decodedData.plan_valid_until) {
-      console.log("No plan was found.");
-      throw new Error("No plan was found.");
-    } else if (moment.utc(decodedData.plan_valid_until).isBefore(moment.utc())) {
-      console.log("Plan has expired.");
-      throw new Error("Plan has expired.");
-    }
-
     request.userData = { eth_wallet_address: decodedData.eth_wallet_address.toLowerCase(),
                          user_id: decodedData.user_id,
                          plan_valid_until: decodedData.plan_valid_until,
